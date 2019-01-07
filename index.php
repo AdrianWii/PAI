bde25a0bd47e7116ca8b8a9898629fb798ae59de
Patryk__ <bielen.patryk@student.pk.edu.pl>
2019-01-07 23:12:54 +0100

    Project creation

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width"
+    <title>Skleplix</title>
+    <link rel="stylesheet" href="sass/main.css">
+</head>
+<body>
+<header>
+    <div class="container">
+
+    </div>
+</header>
+
+</body>
+</html>
